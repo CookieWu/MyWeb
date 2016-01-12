2957c744e3e4fe6f9c836d4dd5d393eb2c3477b9
john <johnwu@iii.org.tw>
2016-01-12 13:52:55 +0800

    DB

--- db/structure.sql
+++ db/structure.sql
@@ -357,6 +357,8 @@ CREATE UNIQUE INDEX unique_schema_migrations ON schema_migrations USING btree (v
 
 SET search_path TO "$user",public;
 
+INSERT INTO schema_migrations (version) VALUES ('20160106055119');
+
 INSERT INTO schema_migrations (version) VALUES ('20160107033811');
 
 INSERT INTO schema_migrations (version) VALUES ('20160107040844');
